2d02144220ea539bff032e909c18c22fbfba95b7
Inge Saunders <ingesaunders@gmail.com>
2019-07-28 16:36:17 +0200

    Initial

---
+++ reportcomments.js
@@ -0,0 +1,423 @@
+const spec = [{
+    "name": "characteristics",
+    "section_label": "Characteristics",
+    "template": "is particularly {{ combined }}",
+    "items": [
+        {
+            "name": "kind",
+            "short_label": "Kind",
+            "full_label": "kind",
+        },
+        {
+            "name": "funny",
+            "short_label": "Funny",
+            "full_label": "funny",
+        },
+        {
+            "name": "thoughtful",
+            "short_label": "Thoughtful",
+            "full_label": "thoughful",
+        },
+        {
+            "name": "friendly",
+            "short_label": "Friendly",
+            "full_label": "friendly",
+        },
+        {
+            "name": "sweet",
+            "short_label": "Sweet",
+            "full_label": "sweet",
+
+        },
+        {
+            "name": "confident",
+            "short_label": "Confident",
+            "full_label": "confident",
+        },
+        {
+            "name": "strong-willed",
+            "short_label": "Strong-willed",
+            "full_label": "strong-willed",
+        },
+        {
+            "name": "quiet",
+            "short_label": "Quiet",
+            "full_label": "quiet",
+        },
+        {
+            "name": "creative",
+            "short_label": "Creative",
+            "full_label": "creative",
+        },
+        {
+            "name": "enthusiastic",
+            "short_label": "Enthusiastic",
+            "full_label": "enthusiastic",
+        },
+        {
+            "name": "responsible",
+            "short_label": "Responsible",
+            "full_label": "responsible",
+        }
+    ]
+},
+
+{
+    "name": "strengths",
+    "section_label": "Strengths",
+    "template": "has made good progress in {{ combined }}.",
+    "items": [
+        {
+            "name": "spelling",
+            "short_label": "Spelling",
+            "full_label": "spelling",
+        },
+        {
+            "name": "reading",
+            "short_label": "Reading",
+            "full_label": "reading",
+        },
+        {
+            "name": "reading_comprehension",
+            "short_label": "Reading Comprehension",
+            "full_label": "reading comprehension",
+        },
+        {
+            "name": "responsible",
+            "short_label": "Responsible",
+            "full_label": "responsible",
+        },
+        {
+            "name": "creative_writing",
+            "short_label": "Creative Writing",
+            "full_label": "creative writing",
+        },
+        {
+            "name": "language_use",
+            "short_label": "Language Use",
+            "full_label": "language use",
+        },
+        {
+            "name": "speaking",
+            "short_label": "Speaking",
+            "full_label": "speaking",
+        },
+        {
+            "name": "number_operations",
+            "short_label": "Number Operations",
+            "full_label": "number operations",
+        },
+        {
+            "name": "patterns",
+            "short_label": "Patterns",
+            "full_label": "patterns",
+        },
+        {
+            "name": "space_and_shape",
+            "short_label": "Space and Shape",
+            "full_label": "space and shape",
+        },
+        {
+            "name": "measurement",
+            "short_label": "Measurement",
+            "full_label": "measurement",
+        },
+        {
+            "name": "data_handling",
+            "short_label": "Data Handling",
+            "full_label": "data handling",
+        },
+        {
+            "name": "afrikaans_reading",
+            "short_label": "Afrikaans Reading",
+            "full_label": "afrikaans reading",
+        },
+        {
+            "name": "afrikaans_spelling",
+            "short_label": "Afrikaans Spelling",
+            "full_label": "afrikaans spelling",
+        },
+        {
+            "name": "afrikaans_writing",
+            "short_label": "Afrikaans Writing",
+            "full_label": "afrikaans writing",
+        },
+        {
+            "name": "afrikaans_speaking",
+            "short_label": "Afrikaans Speaking",
+            "full_label": "afrikaans speaking",
+        },
+        {
+            "name": "art",
+            "short_label": "Art",
+            "full_label": "art",
+        },
+        {
+            "name": "music",
+            "short_label": "Music",
+            "full_label": "music",
+        }
+    ]
+},
+{
+    "name": "weaknesses",
+    "section_label": "Weaknesses",
+    "template": "Next term I would like to see him work on {{ combined }} more often.",
+    "items": [
+        {
+            "name": "spelling",
+            "short_label": "Spelling",
+            "full_label": "spelling",
+        },
+        {
+            "name": "reading_decoding",
+            "short_label": "Reading: Decoding",
+            "full_label": "reading: decoding",
+        },
+        {
+            "name": "fluency_and_word_recognition",
+            "short_label": "Reading: Fluency and Word Recognition",
+            "full_label": "reading: decoding",
+        },
+        {
+            "name": "reading_comprehension",
+            "short_label": "Reading Comprehension",
+            "full_label": "reading comprehension",
+        },
+        {
+            "name": "responsible",
+            "short_label": "Responsible",
+            "full_label": "responsible",
+        },
+        {
+            "name": "creative_writing",
+            "short_label": "Creative Writing",
+            "full_label": "creative writing",
+        },
+        {
+            "name": "language_use",
+            "short_label": "Language Use",
+            "full_label": "language use",
+        },
+        {
+            "name": "speaking",
+            "short_label": "Speaking",
+            "full_label": "speaking",
+        },
+        {
+            "name": "number_operations",
+            "short_label": "Number Operations",
+            "full_label": "number operations",
+        },
+        {
+            "name": "patterns",
+            "short_label": "Patterns",
+            "full_label": "patterns",
+        },
+        {
+            "name": "space_and_shape",
+            "short_label": "Space and Shape",
+            "full_label": "space and shape",
+        },
+        {
+            "name": "measurement",
+            "short_label": "Measurement",
+            "full_label": "measurement",
+        },
+        {
+            "name": "data_handling",
+            "short_label": "Data Handling",
+            "full_label": "data handling",
+        },
+        {
+            "name": "afrikaans_reading",
+            "short_label": "Afrikaans Reading",
+            "full_label": "afrikaans reading",
+        },
+        {
+            "name": "afrikaans_spelling",
+            "short_label": "Afrikaans Spelling",
+            "full_label": "afrikaans spelling",
+        },
+        {
+            "name": "afrikaans_writing",
+            "short_label": "Afrikaans Writing",
+            "full_label": "afrikaans writing",
+        },
+        {
+            "name": "afrikaans_speaking",
+            "short_label": "Afrikaans Speaking",
+            "full_label": "afrikaans speaking",
+        },
+        {
+            "name": "art",
+            "short_label": "Art",
+            "full_label": "art",
+        },
+        {
+            "name": "music",
+            "short_label": "Music",
+            "full_label": "music",
+        },
+        {
+            "name": "listening_skills",
+            "short_label": "Listening Skills",
+            "full_label": "listening skills",
+        },
+        {
+            "name": "attention",
+            "short_label": "Attention",
+            "full_label": "attention",
+        }
+    ]
+},
+{
+    "name": "focus_area",
+    "section_label": "Focus Area",
+    "template": "is finding {{ combined }} challenging.",
+    "items": [
+        {
+            "name": "spelling",
+            "short_label": "Spelling",
+            "full_label": "spelling",
+        },
+        {
+            "name": "reading_decoding",
+            "short_label": "Reading: Decoding",
+            "full_label": "reading: decoding",
+        },
+        {
+            "name": "fluency_and_word_recognition",
+            "short_label": "Reading: Fluency and Word Recognition",
+            "full_label": "reading: decoding",
+        },
+        {
+            "name": "reading_comprehension",
+            "short_label": "Reading Comprehension",
+            "full_label": "reading comprehension",
+        },
+        {
+            "name": "responsible",
+            "short_label": "Responsible",
+            "full_label": "responsible",
+        },
+        {
+            "name": "creative_writing",
+            "short_label": "Creative Writing",
+            "full_label": "creative writing",
+        },
+        {
+            "name": "language_use",
+            "short_label": "Language Use",
+            "full_label": "language use",
+        },
+        {
+            "name": "speaking",
+            "short_label": "Speaking",
+            "full_label": "speaking",
+        },
+        {
+            "name": "number_operations",
+            "short_label": "Number Operations",
+            "full_label": "number operations",
+        },
+        {
+            "name": "patterns",
+            "short_label": "Patterns",
+            "full_label": "patterns",
+        },
+        {
+            "name": "space_and_shape",
+            "short_label": "Space and Shape",
+            "full_label": "space and shape",
+        },
+        {
+            "name": "measurement",
+            "short_label": "Measurement",
+            "full_label": "measurement",
+        },
+        {
+            "name": "data_handling",
+            "short_label": "Data Handling",
+            "full_label": "data handling",
+        },
+        {
+            "name": "afrikaans_reading",
+            "short_label": "Afrikaans Reading",
+            "full_label": "afrikaans reading",
+        },
+        {
+            "name": "afrikaans_spelling",
+            "short_label": "Afrikaans Spelling",
+            "full_label": "afrikaans spelling",
+        },
+        {
+            "name": "afrikaans_writing",
+            "short_label": "Afrikaans Writing",
+            "full_label": "afrikaans writing",
+        },
+        {
+            "name": "afrikaans_speaking",
+            "short_label": "Afrikaans Speaking",
+            "full_label": "afrikaans speaking",
+        },
+        {
+            "name": "art",
+            "short_label": "Art",
+            "full_label": "art",
+        },
+        {
+            "name": "music",
+            "short_label": "Music",
+            "full_label": "music",
+        },
+        {
+            "name": "listening_skills",
+            "short_label": "Listening Skills",
+            "full_label": "listening skills",
+        },
+        {
+            "name": "attention",
+            "short_label": "Attention",
+            "full_label": "attention",
+        }
+    ]
+}
+
+];
+
+function generateForm(spec) {
+    spec.forEach(function (section) {
+        insertHeading(section.section_label);
+
+        section.items.forEach(function (item) {
+            insertCheckbox(item);
+        });
+    });
+}
+
+function generateReport(spec, form) {
+    spec.forEach(function (section) {
+        const sections = [];
+
+        section.items.forEach(function (item) {
+            const answer = getAnswer(item);
+            if (answer) selections.push(answer);
+        })
+        writeSentence(section, selections);
+    });
+}
+
+function getAnswer(item) {
+    console.log("hello");
+}
+
+function insertHeading(heading) {
+    document.getElementById("form-container").innerHTML += '<h1>'+heading+'</h1>';
+}
+
+function insertCheckbox(item) {
+    document.getElementById("checkboxes").innerHTML += '<input type="checkbox">'
+}
+
+generateForm(spec);
