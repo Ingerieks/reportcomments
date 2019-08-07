d73905ae4893533a933b9d78f580993bf3e1d43b
Inge Saunders <ingesaunders@gmail.com>
2019-08-07 19:40:19 +0200

    Paragraph edit

--- reportcomments.js
+++ reportcomments.js
@@ -1,63 +1,68 @@
 const spec = [{
     "name": "characteristics",
     "section_label": "Characteristics",
-    "template": (combined) =>  `is particularly ${combined}`,
+    "template": (combined) =>  `(name) is a very ${combined}boy.`,
     "items": [
         {
-            "name": "kind",
+            "name": "kind ",
             "short_label": "Kind",
             "full_label": "kind",
         },
         {
-            "name": "funny",
-            "short_label": "Funny",
+            "name": "funny ",
+            "short_label": "Funny ",
             "full_label": "has a great sense of humor",
         },
         {
-            "name": "thoughtful",
-            "short_label": "Thoughtful",
-            "full_label": "thoughful",
+            "name": "thoughtful ",
+            "short_label": "Thoughtful ",
+            "full_label": "thoughtful ",
         },
         {
-            "name": "friendly",
-            "short_label": "Friendly",
-            "full_label": "friendly",
+            "name": "friendly ",
+            "short_label": "Friendly ",
+            "full_label": "friendly ",
         },
         {
-            "name": "sweet",
-            "short_label": "Sweet",
+            "name": "sweet ",
+            "short_label": "Sweet ",
             "full_label": "sweet",
 
         },
         {
-            "name": "confident",
-            "short_label": "Confident",
-            "full_label": "confident",
+            "name": "confident ",
+            "short_label": "Confident ",
+            "full_label": "confident ",
         },
         {
-            "name": "strong-willed",
-            "short_label": "Strong-willed",
-            "full_label": "strong-willed",
+            "name": "playful ",
+            "short_label": "Playful ",
+            "full_label": "playful ",
         },
         {
-            "name": "quiet",
-            "short_label": "Quiet",
-            "full_label": "quiet",
+            "name": "strong-willed ",
+            "short_label": "Strong-willed ",
+            "full_label": "strong-willed ",
         },
         {
-            "name": "creative",
-            "short_label": "Creative",
-            "full_label": "creative",
+            "name": "quiet ",
+            "short_label": "Quiet ",
+            "full_label": "quiet ",
         },
         {
-            "name": "enthusiastic",
-            "short_label": "Enthusiastic",
-            "full_label": "enthusiastic",
+            "name": "creative ",
+            "short_label": "Creative ",
+            "full_label": "imaginative ",
         },
         {
-            "name": "responsible",
-            "short_label": "Responsible",
-            "full_label": "responsible",
+            "name": "enthusiastic ",
+            "short_label": "Enthusiastic ",
+            "full_label": "enthusiastic ",
+        },
+        {
+            "name": "responsible ",
+            "short_label": "Responsible ",
+            "full_label": "responsible ",
         }
     ]
 },
@@ -65,7 +70,7 @@ const spec = [{
 {
     "name": "strengths",
     "section_label": "Strengths",
-    "template": (combined) =>  `has made good progress in ${combined}.`,
+    "template": (combined) =>  `He has a good grasp of the concepts taught in ${combined}.`,
     "items": [
         {
             "name": "spelling",
@@ -83,9 +88,96 @@ const spec = [{
             "full_label": "reading comprehension",
         },
         {
-            "name": "responsible",
-            "short_label": "Responsible",
-            "full_label": "responsible",
+            "name": "creative_writing",
+            "short_label": "Creative Writing",
+            "full_label": "creative writing",
+        },
+        {
+            "name": "language_use",
+            "short_label": "Language Use",
+            "full_label": "language use",
+        },
+        {
+            "name": "speaking",
+            "short_label": "Speaking",
+            "full_label": "speaking",
+        },
+        {
+            "name": "number_operations",
+            "short_label": "Number Operations",
+            "full_label": "number operations",
+        },
+        {
+            "name": "patterns",
+            "short_label": "Patterns",
+            "full_label": "patterns",
+        },
+        {
+            "name": "space_and_shape",
+            "short_label": "Space and Shape",
+            "full_label": "space and shape",
+        },
+        {
+            "name": "measurement",
+            "short_label": "Measurement",
+            "full_label": "measurement",
+        },
+        {
+            "name": "data_handling",
+            "short_label": "Data Handling",
+            "full_label": "data handling",
+        },
+        {
+            "name": "afrikaans_reading",
+            "short_label": "Afrikaans Reading",
+            "full_label": "afrikaans reading",
+        },
+        {
+            "name": "afrikaans_spelling",
+            "short_label": "Afrikaans Spelling",
+            "full_label": "afrikaans spelling",
+        },
+        {
+            "name": "afrikaans_writing",
+            "short_label": "Afrikaans Writing",
+            "full_label": "afrikaans writing",
+        },
+        {
+            "name": "afrikaans_speaking",
+            "short_label": "Afrikaans Speaking",
+            "full_label": "afrikaans speaking",
+        },
+        {
+            "name": "art",
+            "short_label": "Art",
+            "full_label": "art",
+        },
+        {
+            "name": "music",
+            "short_label": "Music",
+            "full_label": "music",
+        }
+    ]
+},
+{
+    "name": "progress",
+    "section_label": "Progressed in",
+    "template": (combined) =>  `He is making good progress in ${combined} and he is learning to apply new skills learned in this area, independently.`,
+    "items": [
+        {
+            "name": "spelling",
+            "short_label": "Spelling",
+            "full_label": "spelling",
+        },
+        {
+            "name": "reading",
+            "short_label": "Reading",
+            "full_label": "reading",
+        },
+        {
+            "name": "reading_comprehension",
+            "short_label": "Reading Comprehension",
+            "full_label": "reading comprehension",
         },
         {
             "name": "creative_writing",
@@ -162,7 +254,7 @@ const spec = [{
 {
     "name": "weaknesses",
     "section_label": "Weaknesses",
-    "template": (combined) =>  `is finding ${combined} challenging and would benefit from more practice in this area.`,
+    "template": (combined) =>  `(name) is still finding ${combined} challenging and he will benefit from practicing these concepts daily.`,
     "items": [
         {
             "name": "spelling",
@@ -183,11 +275,6 @@ const spec = [{
             "name": "reading_comprehension",
             "short_label": "Reading Comprehension",
             "full_label": "reading comprehension",
-        },
-        {
-            "name": "responsible",
-            "short_label": "Responsible",
-            "full_label": "responsible",
         },
         {
             "name": "creative_writing",
@@ -274,92 +361,87 @@ const spec = [{
 {
     "name": "focus_area",
     "section_label": "Focus Area",
-    "template": (combined) => `Next term, I would like to see him working on ${combined} .`,
+    "template": (combined) => `Next term, I would like to see him ${combined} .`,
     "items": [
         {
             "name": "spelling",
             "short_label": "Spelling",
-            "full_label": "spelling",
+            "full_label": "practising and applying phonetic strategies",
         },
         {
             "name": "reading_decoding",
             "short_label": "Reading: Decoding",
-            "full_label": "reading: decoding",
+            "full_label": "practising decoding strategies, such as breaking up bigger words into more readable parts",
         },
         {
             "name": "fluency_and_word_recognition",
             "short_label": "Reading: Fluency and Word Recognition",
-            "full_label": "reading: decoding",
+            "full_label": "improving his reading fluency by more consistent reading at home and practising sight words regularly",
         },
         {
             "name": "reading_comprehension",
             "short_label": "Reading Comprehension",
-            "full_label": "reading comprehension",
-        },
-        {
-            "name": "responsible",
-            "short_label": "Responsible",
-            "full_label": "responsible",
+            "full_label": "working on his reading comprehension by practising and internalising the reading strategies taught",
         },
         {
             "name": "creative_writing",
             "short_label": "Creative Writing",
-            "full_label": "creative writing",
+            "full_label": "improving his sentence construction and use of punctuation marks during creative writing",
         },
         {
             "name": "language_use",
             "short_label": "Language Use",
-            "full_label": "language use",
+            "full_label": "work on memorising the different parts of speechs and using the correct punctuation marks more consistently",
         },
         {
             "name": "speaking",
             "short_label": "Speaking",
-            "full_label": "speaking",
+            "full_label": "work on speaking more confidently during oral presentations by speaking louder and making consistent eye contact with the audience",
         },
         {
             "name": "number_operations",
             "short_label": "Number Operations",
-            "full_label": "number operations",
+            "full_label": "practise number facts and skip counting daily by making use of songs, number charts and number lines",
         },
         {
             "name": "patterns",
             "short_label": "Patterns",
-            "full_label": "patterns",
+            "full_label": "practise completing moving and growing patterns, using concrete objects",
         },
         {
             "name": "space_and_shape",
             "short_label": "Space and Shape",
-            "full_label": "space and shape",
+            "full_label": "practise the spelling of 3-D objects and memorising properties of different 3-D objects",
         },
         {
             "name": "measurement",
             "short_label": "Measurement",
-            "full_label": "measurement",
+            "full_label": "practise telling the time daily, working with number lines and using different measurement tools",
         },
         {
-            "name": "data_handling",
-            "short_label": "Data Handling",
-            "full_label": "data handling",
+            "name": "word_problems",
+            "short_label": "Word Problems",
+            "full_label": "work on solving word problems by practising and applying different strategies taught in class",
         },
         {
             "name": "afrikaans_reading",
             "short_label": "Afrikaans Reading",
-            "full_label": "afrikaans reading",
+            "full_label": "taking out Afrikaans reading books from the Library and practising Afrikaans sight words regularly",
         },
         {
             "name": "afrikaans_spelling",
             "short_label": "Afrikaans Spelling",
-            "full_label": "afrikaans spelling",
+            "full_label": "practise Afrikaans spelling more consistently in order to improve his Afrikaans phonic skills",
         },
         {
             "name": "afrikaans_writing",
             "short_label": "Afrikaans Writing",
-            "full_label": "afrikaans writing",
+            "full_label": "work on writing sentences in Afrikaans by using a sentence patterning chart to write simple sentences about a picture",
         },
         {
             "name": "afrikaans_speaking",
             "short_label": "Afrikaans Speaking",
-            "full_label": "afrikaans speaking",
+            "full_label": "work on his Afrikaans speaking by practising more consistently for Afrikaans oral presentations and reading more Afrikaans stories",
         },
         {
             "name": "art",
@@ -374,7 +456,7 @@ const spec = [{
         {
             "name": "listening_skills",
             "short_label": "Listening Skills",
-            "full_label": "listening skills",
+            "full_label": "work on developing his listening skills in order to follow and understand instructions more regularly",
         },
         {
             "name": "attention",
@@ -382,7 +464,51 @@ const spec = [{
             "full_label": "attention",
         }
     ]
+}, 
+{
+    "name": "sign_off",
+    "section_label": "Sign Off",
+    "template": (combined) => ` ${combined} `,
+    "items": [
+        {
+            "name": "Keep up the hard work",
+            "short_label": "Keep up the hard work",
+            "full_label": "Keep up the hard work, (name)!",
+        },
+        {
+            "name": "Supera Moras",
+            "short_label": "Supera Moras",
+            "full_label": "Supera Moras, (name)!",
+        },
+        {
+            "name": "Reach for the stars",
+            "short_label": "Reach for the stars",
+            "full_label": "Reach for the stars, (name)!",
+        },
+        {
+            "name": "Well done on a successful term",
+            "short_label": "Well done on a successful term",
+            "full_label": "Well done on a successful term, (name)!",
+        },
+        {
+            "name": "Show me your best every day. Well done",
+            "short_label": "!Show me your best every day. Well done",
+            "full_label": "!Show me your best every day, (name). Well done!",
+        },
+        {
+            "name": "You can do it",
+            "short_label": "You can do it",
+            "full_label": "You can do it, (name)!",
+        },
+        {
+            "name": "Keep trying your best",
+            "short_label": "Keep trying your best",
+            "full_label": " Keep trying your best, (name)!",
+        }
+       
+     ]
 }
+
 
 ];
 
